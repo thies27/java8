47685d21919bba1e8162c4d22a2dea576740b300
thies27 <g270162@yahoo.de>
2017-11-29 19:06:50 +0100

    Projekt Java 8 angelegt

---
+++ src/de/test/TestMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.test;
+
+/**
+ * @author Mathias
+ *
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Start Erforschung Java 8");
+
+	}
+
+}
