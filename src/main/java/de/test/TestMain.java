4d31e73673a60471df9d75525e31c9d4f8cfb10b
thies27 <g270162@yahoo.de>
2018-02-19 20:14:35 +0100

    19.02.2018

--- src/main/java/de/test/TestMain.java
+++ src/main/java/de/test/TestMain.java
@@ -3,6 +3,9 @@
  */
 package de.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Mathias
  *
@@ -13,15 +16,25 @@ public class TestMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		List<Integer> numbers = new ArrayList<>();
+
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(45);
+		numbers.add(55);
+
+		numbers.forEach(System.out::println);
+		System.out.println("----------------------------------");
 		
-		System.out.println("Start Erforschung Java 8");
+		numbers.forEach(g -> System.out.println(g));
+		System.out.println("----------------------------------");
 		
-		System.out.println("Erforschung in Linux hat geklappt ");
+		numbers.forEach((Integer g) -> System.out.println(g+1));
+		System.out.println("----------------------------------");
 		
-		System.out.println("nochmal Linux - hier jetzt auch mit Eclipse Neon");
-		
-		System.out.println("");
-		
+		numbers.forEach(g -> System.out.println(g+1));
 
 	}
 
